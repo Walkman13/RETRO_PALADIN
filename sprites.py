347b11823b5771adb2d7fe18ce1149e0ca76f08d
freesay <freesay@inbox.ru>
2021-02-10 00:03:35 +0300

    jump animated

--- sprites.py
+++ sprites.py
@@ -40,6 +40,7 @@ class Player(pg.sprite.Sprite):
         for frame in self.standing_frames_l:
             frame.set_colorkey(BLACK)
             self.standing_frames_r.append(pg.transform.flip(frame, True, False))
+
         self.walk_frames_l = [self.game.spritesheet.get_image(0, 48, 48, 48),
                             self.game.spritesheet.get_image(48, 48, 48, 48),
                             self.game.spritesheet.get_image(96, 48, 48, 48),
@@ -49,15 +50,20 @@ class Player(pg.sprite.Sprite):
             frame.set_colorkey(BLACK)
             self.walk_frames_r.append(pg.transform.flip(frame, True, False))
 
+        self.jump_frames_l = [self.game.spritesheet.get_image(0, 96, 48, 48),
+                              self.game.spritesheet.get_image(48, 96, 48, 48)]
+        self.jump_frames_r = []
+        for frame in self.jump_frames_l:
+            frame.set_colorkey(BLACK)
+            self.jump_frames_r.append(pg.transform.flip(frame, True, False))
+
     def jump_cut(self):
         if self.jumping:
             if self.vel.y < -3:
                 self.vel.y = -3
 
     def jump(self):
-        self.rect.x += 1
         hits = pg.sprite.spritecollide(self, self.game.platforms, False)
-        self.rect.x -= 1
         if hits and not self.jumping:
             self.jumping = True
             self.vel.y = -PLAYER_JUMP
@@ -96,7 +102,7 @@ class Player(pg.sprite.Sprite):
             self.walking = False
 
         if self.walking:
-            if now - self.last_update > 180:
+            if now - self.last_update > 120:
                 self.last_update = now
                 self.current_frame = (self.current_frame + 1) % len(self.walk_frames_l)
                 bottom = self.rect.bottom
@@ -107,8 +113,28 @@ class Player(pg.sprite.Sprite):
                 self.rect = self.image.get_rect()
                 self.rect.bottom = bottom
 
+        # OMG!! Refactor this!!
+        if self.jumping:
+            bottom = self.rect.bottom
+            if not self.fliping:
+                if self.vel.y < 0:
+                    self.current_frame = 0
+                    self.image = self.jump_frames_l[self.current_frame]
+                else:
+                    self.current_frame = 1
+                    self.image = self.jump_frames_l[self.current_frame]
+            else:
+                if self.vel.y < 0:
+                    self.current_frame = 0
+                    self.image = self.jump_frames_r[self.current_frame]
+                else:
+                    self.current_frame = 1
+                    self.image = self.jump_frames_r[self.current_frame]
+            self.rect = self.image.get_rect()
+            self.rect.bottom = bottom
+
         if not self.jumping and not self.walking:
-            if now - self.last_update > 350:
+            if now - self.last_update > 250:
                 self.last_update = now
                 self.current_frame = (self.current_frame + 1) % len(self.standing_frames_l)
                 bottom = self.rect.bottom
@@ -120,7 +146,7 @@ class Player(pg.sprite.Sprite):
                 self.rect.bottom = bottom
 
 
-class Platform(pg.sprite.Sprite):
+class Ground(pg.sprite.Sprite):
     def __init__(self, x, y, w, h):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((w, h))
